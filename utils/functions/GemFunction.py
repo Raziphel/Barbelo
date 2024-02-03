8f6004f5de311b34fd533bf5dd6f9835aaeafbe0
Raziphel <raziphel@gmail.com>
2024-02-03 03:38:18 -0600

    First testing for updating gems function.

--- utils/functions/GemFunction.py
+++ utils/functions/GemFunction.py
@@ -1,6 +1,8 @@
 # Discord
 from discord import Member
 
+from math import floor
+
 import utils
 
 class GemFunction(object):
@@ -10,39 +12,38 @@ class GemFunction(object):
 
     @classmethod
     async def update_gems(cls, user:Member):
-        c = utils.Currency.get(user.id)
+        g = utils.Gems.get(user.id)
 
-        #! push coins to next tiers
-        if c.silver >= 100:
-            c.gold += 1
-            c.silver -= 100
+        #+ Upgrade gems to the next tier!
 
-        if c.gold >= 100:
-            c.emerald += 1
-            c.gold -= 100
-
-        if c.emerald >= 100:
-            c.diamond += 1
-            c.emerald -= 100
-
-        if c.diamond >= 100:
-            c.ruby += 1
-            c.diamond -= 100
-
-        if c.ruby >= 100:
-            c.sapphire += 1
-            c.ruby -= 100
-
-        if c.sapphire >= 100:
-            c.amethyst += 1
-            c.sapphire -= 100
-
-        if c.amethyst >= 100:
-            c.phelstone += 1
-            c.amethyst -= 100
+        #? Emerald
+        if g.emerald > 100:
+            for x in range(floor(g.emerald/100)):
+                g.emerald -= 100
+                g.diamond += 1
+        #? Diamond
+        if g.diamond > 100:
+            for x in range(floor(g.diamond/100)):
+                g.diamond -= 100
+                g.ruby += 1
+        #? Ruby
+        if g.ruby > 100:
+            for x in range(floor(g.ruby/100)):
+                g.ruby -= 100 
+                g.sapphire += 1
+        #? Sapphire
+        if g.sapphire > 100:
+            for x in range(floor(g.sapphire/100)):
+                g.sapphire -= 100
+                g.amethyst += 1
+        #? Amethyst
+        if g.amethyst > 100:
+            for x in range(floor(g.amethyst/100)):
+                g.amethyst -= 100
+                g.hellstone += 1
 
         async with cls.bot.database() as db:
-            await c.save(db)
+            await g.save(db)
 
         return
 
