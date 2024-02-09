5196318910c7d35bdc61c4b156b2e22d7c4f1bb7
Raziphel <raziphel@gmail.com>
2024-02-09 08:25:41 -0600

    Perfected the gem functions!

--- utils/functions/gems.py
+++ utils/functions/gems.py
@@ -21,26 +21,45 @@ class GemFunctions(object):
             for x in range(floor(g.emerald/100)):
                 g.emerald -= 100
                 g.diamond += 1
+        elif g.emerald < 0 and g.diamond > 0:
+            g.emerald += 100
+            g.diamond -= 1
+
         #? Diamond
         if g.diamond > 100:
             for x in range(floor(g.diamond/100)):
                 g.diamond -= 100
                 g.ruby += 1
+        elif g.diamond < 0 and g.ruby > 0:
+            g.diamond += 100
+            g.ruby -= 1
+
         #? Ruby
         if g.ruby > 100:
             for x in range(floor(g.ruby/100)):
                 g.ruby -= 100 
                 g.sapphire += 1
+        elif g.ruby < 0 and g.sapphire > 0:
+            g.ruby += 100
+            g.sapphire -= 1
+
         #? Sapphire
         if g.sapphire > 100:
             for x in range(floor(g.sapphire/100)):
                 g.sapphire -= 100
                 g.amethyst += 1
+        elif g.sapphire < 0 and g.amethyst > 0:
+            g.sapphire += 100
+            g.amethyst -= 1
+
         #? Amethyst
         if g.amethyst > 100:
             for x in range(floor(g.amethyst/100)):
                 g.amethyst -= 100
                 g.hellstone += 1
+        elif g.amethyst < 0 and g.hellstone > 0:
+            g.amethyst += 100
+            g.hellstone -= 1
 
         async with cls.bot.database() as db:
             await g.save(db)
@@ -48,36 +67,62 @@ class GemFunctions(object):
 
 
     @classmethod
-    async def payment(cls, user:Member, gem:str):
+    async def payment(cls, user:Member, gem:str, amount:int):
         '''Pay for stuff with gems if they can afford it!  
-        Returns true if it was purchased.'''
+        Returns false if it couldn't be purchased.'''
 
         g = utils.Gems.get(user.id)
 
         #? Hellstone
-        if g.hellstone > 0:
-            g.hellstone -= 1
-            g.amethyst += 100
+        if gem is "hellstone":
+            if g.hellstone >= amount:
+                g.hellstone -= amount
+            else: return False
         #? Amethyst
-        if g.amethyst > 0:
-            g.amethyst -= 1
-            g.sappire += 100
+        if gem is "amethyst":
+            if g.amethyst >= amount:
+                g.amethyst -= amount
+            elif g.hellstone > 0:
+                g.hellstone -= 1
+                g.amethyst += 100
+                g.amethyst -= amount
+            else: return False
         #? Sapphire
-        if g.sapphire > 0:
-            g.sapphire -= 1
-            g.ruby += 100
+        if gem is "sapphire":
+            if g.sapphire >= amount:
+                g.sapphire -= amount
+            elif g.amethyst > 0:
+                g.amethyst -= 1
+                g.sapphire += 100
+                g.sapphire -= amount
+            else: return False
         #? Ruby
-        if g.ruby > 0:
-            g.ruby -= 1
-            g.diamond += 100
+        if gem is "ruby":
+            if g.ruby >= amount:
+                g.ruby -= amount
+            elif g.sapphire > 0:
+                g.sapphire -= 1
+                g.ruby += 100
+                g.ruby -= amount
+            else: return False
         #? Diamond
-        if g.diamond > 0:
-            g.diamond -= 1
-            g.emerald += 100
+        if gem is "diamond":
+            if g.diamond >= amount:
+                g.diamond -= amount
+            elif g.ruby > 0:
+                g.ruby -= 1
+                g.diamond += 100
+                g.diamond -= amount
+            else: return False
         #? Emerald
-        if g.emerald > 0:
-            g.emerald -= 1
-            g.gold += 100
+        if gem is "emerald":
+            if g.emerald >= amount:
+                g.emerald -= amount
+            elif g.diamond > 0:
+                g.diamond -= 1
+                g.emerald += 100
+                g.emerald -= amount
+            else: return False
 
         async with cls.bot.database() as db:
             await g.save(db)
