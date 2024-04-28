ee351aca3fd92338f7bb15dd1ff004130f0c4ea0
Raziphel <raziphel@gmail.com>
2024-04-28 00:50:48 -0500

    AHHHHHHHHHH

--- cogs/users/payment.py
+++ cogs/users/payment.py
@@ -18,99 +18,99 @@ class payment(Cog):
         return self.bot.get_channel(self.bot.config['logs']['gems'])
 
 
-    @cooldown(1, 30, BucketType.user)
-    @command(
-        aliases=['send'],
-        application_command_meta=ApplicationCommandMeta(
-            options=[
-                ApplicationCommandOption(
-                    name="receiver",
-                    description="The user you want to send gems to.",
-                    type=ApplicationCommandOptionType.user,
-                    required=True,
-                ),
-                ApplicationCommandOption(
-                    name="gems",
-                    description="The amount of gems you'd like to send.",
-                    type=ApplicationCommandOptionType.integer,
-                    required=True,
-                ),
-            ],
-        ),
-    )
-    async def pay(self, ctx, receiver:User=None, gems:int=0):
-        '''
-        send payments to other members
-        '''
-        #? Check if bot is connected!
-        if self.bot.connected == False:
-            return
+    # @cooldown(1, 30, BucketType.user)
+    # @command(
+    #     aliases=['send'],
+    #     application_command_meta=ApplicationCommandMeta(
+    #         options=[
+    #             ApplicationCommandOption(
+    #                 name="receiver",
+    #                 description="The user you want to send gems to.",
+    #                 type=ApplicationCommandOptionType.user,
+    #                 required=True,
+    #             ),
+    #             ApplicationCommandOption(
+    #                 name="gems",
+    #                 description="The amount of gems you'd like to send.",
+    #                 type=ApplicationCommandOptionType.integer,
+    #                 required=True,
+    #             ),
+    #         ],
+    #     ),
+    # )
+    # async def pay(self, ctx, receiver:User=None, gems:int=0):
+    #     '''
+    #     send payments to other members
+    #     '''
+    #     #? Check if bot is connected!
+    #     if self.bot.connected == False:
+    #         return
 
-        receiver = utils.DiscordGet(ctx.guild.members, id=receiver)
-        g = utils.Gems.get(ctx.author.id)
-        g_r = utils.Gems.get(receiver.id)
+    #     receiver = utils.DiscordGet(ctx.guild.members, id=receiver)
+    #     g = utils.Gems.get(ctx.author.id)
+    #     g_r = utils.Gems.get(receiver.id)
 
-        if receiver == ctx.author:
-            await ctx.interaction.response.send_message(embed=utils.Embed(desc=f"# {ctx.author.mention} You can't pay yourself gems!", user=ctx.author))
-            return
-        if gems <= 0 or gems > 99:
-            await ctx.interaction.response.send_message(embed=utils.Embed(desc=f"# {ctx.author.mention} Has to be more than 0 and less than 99 gems!", user=ctx.author))
-            return
+    #     if receiver == ctx.author:
+    #         await ctx.interaction.response.send_message(embed=utils.Embed(desc=f"# {ctx.author.mention} You can't pay yourself gems!", user=ctx.author))
+    #         return
+    #     if gems <= 0 or gems > 99:
+    #         await ctx.interaction.response.send_message(embed=utils.Embed(desc=f"# {ctx.author.mention} Has to be more than 0 and less than 99 gems!", user=ctx.author))
+    #         return
 
-        gem_string = await utils.GemFunctions.gems_to_text(diamonds=g.diamond, rubys=g.ruby, sapphires=g.sapphire, amethysts=g.amethyst)
+    #     gem_string = await utils.GemFunctions.gems_to_text(diamonds=g.diamond, rubys=g.ruby, sapphires=g.sapphire, amethysts=g.amethyst)
 
-        embed = utils.Embed(desc=f"# Click the gem you want to send!\n**Here's your current gem count:**\n{gem_string}", user=ctx.author)
-        msg = await ctx.send(embed=embed)
+    #     embed = utils.Embed(desc=f"# Click the gem you want to send!\n**Here's your current gem count:**\n{gem_string}", user=ctx.author)
+    #     msg = await ctx.send(embed=embed)
 
-        # adds the reactions
-        await msg.add_reaction(self.bot.config['gem_emoji']['diamond'])
-        await msg.add_reaction(self.bot.config['gem_emoji']['ruby'])
-        await msg.add_reaction(self.bot.config['gem_emoji']['sapphire'])
-        await msg.add_reaction(self.bot.config['gem_emoji']['amethyst'])
+    #     # adds the reactions
+    #     await msg.add_reaction(self.bot.config['gem_emoji']['diamond'])
+    #     await msg.add_reaction(self.bot.config['gem_emoji']['ruby'])
+    #     await msg.add_reaction(self.bot.config['gem_emoji']['sapphire'])
+    #     await msg.add_reaction(self.bot.config['gem_emoji']['amethyst'])
 
-        try:
+    #     try:
 
-            # Watches for the reactions
-            check = lambda x, y: y.id == ctx.author.id and x.message.id == msg.id and str(x.emoji) in [self.bot.config['gem_emoji']['diamond'], self.bot.config['gem_emoji']['ruby'], self.bot.config['gem_emoji']['sapphire'], self.bot.config['gem_emoji']['amethyst']]
-            r, _ = await self.bot.wait_for('reaction_add', check=check)
-            if str(r.emoji) == self.bot.config['gem_emoji']['diamond']:
-                purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['diamond'], amount=gems)
-                if purchased == False:
-                    raise TooPoor
+    #         # Watches for the reactions
+    #         check = lambda x, y: y.id == ctx.author.id and x.message.id == msg.id and str(x.emoji) in [self.bot.config['gem_emoji']['diamond'], self.bot.config['gem_emoji']['ruby'], self.bot.config['gem_emoji']['sapphire'], self.bot.config['gem_emoji']['amethyst']]
+    #         r, _ = await self.bot.wait_for('reaction_add', check=check)
+    #         if str(r.emoji) == self.bot.config['gem_emoji']['diamond']:
+    #             purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['diamond'], amount=gems)
+    #             if purchased == False:
+    #                 raise TooPoor
 
-            if str(r.emoji) == self.bot.config['gem_emoji']['ruby']:
-                purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['ruby'], amount=gems)
-                if purchased == False:
-                    raise TooPoor
+    #         if str(r.emoji) == self.bot.config['gem_emoji']['ruby']:
+    #             purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['ruby'], amount=gems)
+    #             if purchased == False:
+    #                 raise TooPoor
 
-            if str(r.emoji) == self.bot.config['gem_emoji']['sapphire']:
-                purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['sapphire'], amount=gems)
-                if purchased == False:
-                    raise TooPoor
+    #         if str(r.emoji) == self.bot.config['gem_emoji']['sapphire']:
+    #             purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['sapphire'], amount=gems)
+    #             if purchased == False:
+    #                 raise TooPoor
 
-            if str(r.emoji) == self.bot.config['gem_emoji']['amethyst']:
-                purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['amethyst'], amount=gems)
-                if purchased == False:
-                    raise TooPoor
+    #         if str(r.emoji) == self.bot.config['gem_emoji']['amethyst']:
+    #             purchased = await utils.GemFunctions.payment(user=ctx.author, gem=self.bot.config['gem_emoji']['amethyst'], amount=gems)
+    #             if purchased == False:
+    #                 raise TooPoor
 
 
-        except TooPoor:
-            await ctx.interaction.response.send_message(f'# {ctx.author.mention} Payment denied. You would go in debt stupid!!!')
-            await msg.delete()
-            return
+    #     except TooPoor:
+    #         await ctx.interaction.response.send_message(f'# {ctx.author.mention} Payment denied. You would go in debt stupid!!!')
+    #         await msg.delete()
+    #         return
 
-        #! Always update after paying!
-        await utils.GemFunctions.update(user=ctx.author)
-        await utils.GemFunctions.update(user=receiver)
+    #     #! Always update after paying!
+    #     await utils.GemFunctions.update(user=ctx.author)
+    #     await utils.GemFunctions.update(user=receiver)
 
-        async with self.bot.database() as db:
-            await g.save(db)
-            await g_r.save(db)
+    #     async with self.bot.database() as db:
+    #         await g.save(db)
+    #         await g_r.save(db)
 
-        await msg.delete()
-        await ctx.send(embed=utils.Embed(user=ctx.author, desc=f"# {ctx.author.mention} sent {str(r.emoji)}{gems}x to {receiver.mention}!"))
+    #     await msg.delete()
+    #     await ctx.send(embed=utils.Embed(user=ctx.author, desc=f"# {ctx.author.mention} sent {str(r.emoji)}{gems}x to {receiver.mention}!"))
 
-        await self.gem_logs.send(embed=utils.Embed(user=ctx.author, desc=f"# {ctx.author.mention} sent {gems} {str(r.emoji)}x to {receiver.mention}!"))
+    #     await self.gem_logs.send(embed=utils.Embed(user=ctx.author, desc=f"# {ctx.author.mention} sent {gems} {str(r.emoji)}x to {receiver.mention}!"))
 
 
 
