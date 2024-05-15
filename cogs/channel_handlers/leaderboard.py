20ace2ee117b3b528acd8c31f1e5a124d26f7a40
Raziphel <raziphel@gmail.com>
2024-05-15 02:19:17 -0500

    coin emoji

--- cogs/channel_handlers/leaderboard.py
+++ cogs/channel_handlers/leaderboard.py
@@ -89,9 +89,9 @@ class leaderboard(Cog):
         text2 = []
         for index, (user, rank) in enumerate(zip(users, ranks)):
             if index < 10:
-                text.append(f"#{index+1} **{user.name}** ─── {rank.coins:,}")
+                text.append(f"#{index+1} **{user.name}** ─── {rank.coins:,}{self.bot.config['coin_emoji']}")
             else:
-                text2.append(f"#{index+1} **{user.name}** ─── {rank.coins:,}")
+                text2.append(f"#{index+1} **{user.name}** ─── {rank.coins:,}{self.bot.config['coin_emoji']}")
 
         embed.description = '\n'.join(text)
         embed2.description = '\n'.join(text2)
